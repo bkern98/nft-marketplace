a65f4c5d70afb5a40a66c73bdaa8bd4b37570971
bkern98 <bkern288@gmail.com>
2021-12-02 10:21:13 -0500

    set up hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,21 @@
 require('@nomiclabs/hardhat-waffle');
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+const projectId = '8acaccd5096247d087aa0da6497e2f74';
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: '0.8.4'
 };
