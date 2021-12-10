0feeec626a989ea37a748ef464214e5d04b0385f
bkern98 <bkern288@gmail.com>
2021-12-10 12:09:29 -0500

    compiled artifacts to import into homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-import styles from '../styles/Home.module.css';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Web3Modal from 'web3modal';
+
+import { nftaddress, nftmarketaddress } from '../config';
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
 export default function Home() {
   return (
