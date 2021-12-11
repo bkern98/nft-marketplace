51d07e65971851fceacfd036bfa08d2a5a396def
bkern98 <bkern288@gmail.com>
2021-12-11 15:12:01 -0500

    created creator-dashboard page

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -5,11 +5,12 @@ import Web3Modal from 'web3modal';
 
 import { nftmarketaddress, nftaddress } from '../config';
 
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 
-export default function MyAssets() {
+export default function CreatorDashboard() {
   const [nfts, setNfts] = useState([]);
+  const [sold, setSold] = useState([]);
   const [loadingState, setLoadingState] = useState('not-loaded');
 
   useEffect(() => {
@@ -28,7 +29,7 @@ export default function MyAssets() {
       signer
     );
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-    const data = await marketContract.fetchMyNFTs();
+    const data = await marketContract.fetchItemsCreated();
 
     const items = await Promise.all(
       data.map(async i => {
@@ -40,23 +41,28 @@ export default function MyAssets() {
           tokenId: i.tokenId.toNumber(),
           seller: i.seller,
           owner: i.owner,
+          sold: i.sold,
           image: meta.data.image
         };
 
         return item;
       })
     );
+
+    const soldItems = items.filter(i => i.sold);
+    setSold(soldItems);
     setNfts(items);
     setLoadingState('loaded');
   }
 
   if (loadingState === 'loaded' && !nfts.length) {
-    return <h1 className='py-10 px-20 text-3xl'>No assets owned</h1>;
+    return <h1 className='py-10 px-20 text-3xl'>No assets created</h1>;
   }
 
   return (
-    <div className='flex justify-center'>
+    <div>
       <div className='p-4'>
+        <h2 className='text-2xl py-2'>Items Created</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
           {nfts.map((nft, i) => (
             <div key={i} className='border shadow rounded-xl overflow-hidden'>
@@ -69,6 +75,28 @@ export default function MyAssets() {
             </div>
           ))}
         </div>
+      </div>
+      <div className='px-4'>
+        {Boolean(sold.length) && (
+          <div>
+            <h2 className='text-2xl py-2'>Items sold</h2>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+              {sold.map((nft, i) => (
+                <div
+                  key={i}
+                  className='border shadow rounded-xl overflow-hidden'
+                >
+                  <img src={nft.image} className='rounded' />
+                  <div className='p-4 bg-black'>
+                    <p className='text-2xl font-bold text-white'>
+                      Price - {nft.price} Eth
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
